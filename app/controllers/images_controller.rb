f30acffa253b1ee23a948fc1258f4831528f1756
d.mathisen <drew.a.mathisen@gmail.com>
2022-01-23 18:40:57 -0700

    Created one_random_image route and five_random_images route that pull one and five random images respectively.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
   def index
-    render json: "in index"
+    render json: HTTP.get("https://api.nasa.gov/planetary/apod?api_key=#{Rails.application.credentials.nasa_api_key}&count=1")
   end
 end
